8f003ee4f81a3228a766c8d3004953aa5429baca
dogcane_cp <SND\dogcane_cp>
2014-02-25 14:33:50 +0000

    Better version of WCF integration (Data context)

--- ECO/Core/ECO/WCF/DataContextAttribute.cs
+++ ECO/Core/ECO/WCF/DataContextAttribute.cs
@@ -8,30 +8,80 @@ using System.Threading.Tasks;
 
 namespace ECO.WCF
 {
-    public class DataContextAttribute : Attribute, IParameterInspector, IOperationBehavior
+    public class DataContextOperationInvoker : IOperationInvoker
+    {
+        #region Fields
+
+        private IOperationInvoker _InnerOperationInvoker;
+
+        private bool _RequiredTransaction;
+
+        private bool _AutoCommitTransaction;
+
+        #endregion
+
+        #region Ctor
+
+        public DataContextOperationInvoker(IOperationInvoker operationInvoker, bool requiredTransaction, bool autoCommitTransaction)
+        {
+            _InnerOperationInvoker = operationInvoker;
+            _RequiredTransaction = requiredTransaction;
+            _AutoCommitTransaction = autoCommitTransaction;
+        }
+
+        #endregion
+
+        #region IOperationInvoker Members
+
+        public object[] AllocateInputs()
+        {
+            return _InnerOperationInvoker.AllocateInputs();
+        }
+
+        public object Invoke(object instance, object[] inputs, out object[] outputs)
+        {
+            using (ECO.Data.DataContext dtx = new Data.DataContext())
+            {
+                if (_RequiredTransaction)
+                {
+                    dtx.BeginTransaction(_AutoCommitTransaction);
+                }
+                return _InnerOperationInvoker.Invoke(instance, inputs, out outputs);
+            }
+        }
+
+        public IAsyncResult InvokeBegin(object instance, object[] inputs, AsyncCallback callback, object state)
+        {
+            ECO.Data.DataContext dtx = new Data.DataContext();            
+            if (_RequiredTransaction)
+            {
+                dtx.BeginTransaction(_AutoCommitTransaction);
+            }
+            return _InnerOperationInvoker.InvokeBegin(instance, inputs, callback, state);
+        }
+
+        public object InvokeEnd(object instance, out object[] outputs, IAsyncResult result)
+        {
+            object returnValue = _InnerOperationInvoker.InvokeEnd(instance, out outputs, result);
+            ECO.Data.DataContext.Current.Close();
+            return returnValue;
+        }
+
+        public bool IsSynchronous
+        {
+            get { return _InnerOperationInvoker.IsSynchronous; }
+        }
+
+        #endregion
+    }
+
+    public class DataContextAttribute : Attribute, IOperationBehavior
     {
         public bool RequiredTransaction = false;
 
         public bool AutoCommitTransaction = true;
 
-        #region IParameterInspector Members
-
-        public void AfterCall(string operationName, object[] outputs, object returnValue, object correlationState)
-        {
-            ECO.Data.DataContext.Current.Close();
-        }
-
-        public object BeforeCall(string operationName, object[] inputs)
-        {
-            ECO.Data.DataContext dtx = new Data.DataContext();
-            if (RequiredTransaction)
-            {
-                dtx.BeginTransaction(AutoCommitTransaction);
-            }
-            return inputs;
-        }
-
-        #endregion
+        
 
         #region IOperationBehavior Members
 
@@ -42,12 +92,12 @@ namespace ECO.WCF
 
         public void ApplyClientBehavior(OperationDescription operationDescription, ClientOperation clientOperation)
         {
-            clientOperation.ClientParameterInspectors.Add(this);            
+            
         }
 
         public void ApplyDispatchBehavior(OperationDescription operationDescription, DispatchOperation dispatchOperation)
         {
-            dispatchOperation.ParameterInspectors.Add(this);
+            dispatchOperation.Invoker = new DataContextOperationInvoker(dispatchOperation.Invoker, RequiredTransaction, AutoCommitTransaction);
         }
 
         public void Validate(OperationDescription operationDescription)
