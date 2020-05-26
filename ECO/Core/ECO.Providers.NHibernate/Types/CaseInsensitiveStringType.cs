14137c20a8f4e898d99c518bfd1767173be4e884
andrea.canegrati <andrea.canegrati@CANEGRATI>
2020-05-26 11:54:23 +0200

    - CaseInsensitiveStringType

---
+++ ECO/Core/ECO.Providers.NHibernate/Types/CaseInsensitiveStringType.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Engine;
+
+namespace ECO.Providers.NHibernate.Types
+{
+    public class CaseInsensitiveStringType : global::NHibernate.Type.AbstractStringType
+    {
+        #region Fields
+
+        private StringComparer _Comparer = StringComparer.CurrentCultureIgnoreCase;
+
+        #endregion
+
+        public CaseInsensitiveStringType(global::NHibernate.SqlTypes.SqlType sqlType)
+            : base(sqlType)
+        {
+
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "String";
+            }
+        }
+
+        public override bool IsEqual(object x, object y)
+        {
+            return _Comparer.Equals(x, y);
+        }
+
+        public override bool IsEqual(object x, object y, EntityMode entityMode)
+        {
+            return IsEqual(x, y);
+        }
+
+        public override bool IsEqual(object x, object y, EntityMode entityMode, ISessionFactoryImplementor factory)
+        {
+            return IsEqual(x, y);
+        }
+
+        public override int GetHashCode(object x, EntityMode entityMode)
+        {
+            return _Comparer.GetHashCode(x);
+        }
+
+        public override int GetHashCode(object x, EntityMode entityMode, ISessionFactoryImplementor factory)
+        {
+            return GetHashCode(x, entityMode);
+        }
+    }
+}
---
+++ ECO/Core/ECO/DataContextIsolationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECO
+{
+    /// <summary>
+    /// Type of DataContext instance isolation model
+    /// </summary>
+    public enum DataContextIsolationModel
+    {
+        /// <summary>
+        /// Shared DataContext
+        /// </summary>
+        Standard = 0,
+        /// <summary>
+        /// Isolated DataContext (no enlist in global DataContext)
+        /// </summary>
+        Isolated = 1
+    }
+}
