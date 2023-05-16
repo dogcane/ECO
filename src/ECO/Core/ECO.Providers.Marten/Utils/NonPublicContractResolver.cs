022cf81b6811eb99d37e3e382d1a094c608ee078
Andrea Canegrati <andrea@canegrati.net>
2023-05-16 17:37:02 +0200

    - Change to "NonPublicContractResolver"

--- src/ECO/Core/ECO.Providers.Marten/Utils/NonPublicContractResolver.cs
+++ src/ECO/Core/ECO.Providers.Marten/Utils/NonPublicContractResolver.cs
@@ -8,6 +8,10 @@ namespace ECO.Providers.Marten.Utils
 {
     public class NonPublicContractResolver : DefaultJsonTypeInfoResolver
     {
+        public bool UseParameterlessCtor { get; init; } = true;
+
+        public bool UseFields { get; init; } = true;
+
         public string FieldNamingStragegy { get; init; } = "_{0}";
 
         public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
@@ -15,42 +19,31 @@ namespace ECO.Providers.Marten.Utils
             JsonTypeInfo jsonTypeInfo = base.GetTypeInfo(type, options);
             if (jsonTypeInfo.Kind == JsonTypeInfoKind.Object)
             {
-                //Default Non Public Ctor
-                if (jsonTypeInfo.CreateObject is null && jsonTypeInfo.Type.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Length == 0)
+                if (UseParameterlessCtor)
                 {
-                    // The type doesn't have public constructors
-                    jsonTypeInfo.CreateObject = () =>
-                    {
-                        return Activator.CreateInstance(jsonTypeInfo.Type, true)!;
-                    };                    
+                    //Default Parameterless Ctor
+                    jsonTypeInfo.CreateObject = () => Activator.CreateInstance(jsonTypeInfo.Type, true)!;
                 }
 
-                //All Properties + Field;
-                jsonTypeInfo.Properties.Clear();
-                foreach (PropertyInfo propertyInfo in jsonTypeInfo.Type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                //Try properties/field public/non public resolver
+                foreach (var prop in jsonTypeInfo.Properties)
                 {
-                    JsonPropertyInfo jPropertyInfo = jsonTypeInfo.CreateJsonPropertyInfo(propertyInfo.PropertyType, propertyInfo.Name);
-                    if (propertyInfo.CanRead)
+                    if (prop.Set == null)
                     {
-                        jPropertyInfo.Get = obj => propertyInfo.GetValue(obj);
-                    }
-                    else
-                    {
-
-                    }                    
-                    if (propertyInfo.CanWrite)
-                    {
-                        jPropertyInfo.Set = (obj, value) => propertyInfo.SetValue(obj, value);
-                    }
-                    else
-                    {
-                        FieldInfo? fieldInfo = jsonTypeInfo.Type.GetField(string.Format(FieldNamingStragegy, propertyInfo.Name), BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                        if (fieldInfo != null)
+                        var propertyInfo = jsonTypeInfo.Type.GetProperty(prop.Name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                        if (propertyInfo != null && propertyInfo.CanWrite)
                         {
-                            jPropertyInfo.Set = (obj, value) => fieldInfo.SetValue(obj, value);
+                            prop.Set = (obj, value) => propertyInfo.SetValue(obj, value);
+                        }
+                        else if (UseFields)
+                        {
+                            FieldInfo? fieldInfo = jsonTypeInfo.Type.GetField(string.Format(FieldNamingStragegy, prop.Name), BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                            if (fieldInfo != null)
+                            {
+                                prop.Set = (obj, value) => fieldInfo.SetValue(obj, value);
+                            }
                         }
                     }
-                    jsonTypeInfo.Properties.Add(jPropertyInfo);
                 }
             }
             return jsonTypeInfo;
