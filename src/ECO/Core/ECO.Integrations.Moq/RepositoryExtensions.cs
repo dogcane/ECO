2c768cbb3252d7563efc088c5023a0631997d44f
Andrea Canegrati <andrea@canegrati.net>
2023-07-19 08:50:06 +0200

    - upgrade version - moq testing integrations

--- src/ECO/Core/ECO.Integrations.Moq/RepositoryExtensions.cs
+++ src/ECO/Core/ECO.Integrations.Moq/RepositoryExtensions.cs
@@ -7,41 +7,30 @@ public static class RepositoryExtensions
     public static Mock<T> SetupQueryable<T, TItem>(this Mock<T> queryableMock, IEnumerable<TItem> source)
         where T : class, IEnumerable<TItem>
     {
+
         var queryableList = source.AsQueryable();
+
         queryableMock.As<IQueryable<TItem>>().Setup(x => x.Provider).Returns(queryableList.Provider);
         queryableMock.As<IQueryable<TItem>>().Setup(x => x.Expression).Returns(queryableList.Expression);
         queryableMock.As<IQueryable<TItem>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
         queryableMock.As<IQueryable<TItem>>().Setup(x => x.GetEnumerator()).Returns(() => queryableList.GetEnumerator());
+
         return queryableMock;
     }
 
     public static Mock<T> SetupRepository<T, TItem, TKey>(this Mock<T> repository, IList<TItem> source)
-    where T : class, IRepository<TItem, TKey>
-    where TItem : class, IAggregateRoot<TKey>
+        where T : class, IRepository<TItem, TKey>
+        where TItem : class, IAggregateRoot<TKey>
     {
         repository.SetupReadOnlyRepository<T, TItem, TKey>(source);
         repository.Setup(obj => obj.Add(It.IsAny<TItem>())).Callback((TItem item) => {
-            source.Add(item);            
-            var identityValue = typeof(TKey) switch
-            {
-                Type t when t == typeof(int) => (TKey)(object)source.Count,
-                Type t when t == typeof(long) => (TKey)(object)source.Count,
-                Type t when t == typeof(Guid) => (TKey)(object)Guid.NewGuid(),
-                _ => default
-            };
-            item.GetType().GetProperty("Identity", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)!.SetValue(item, identityValue);
+            source.Add(item);
+            item.SetMockedIdentity<TItem, TKey>(source);
         });
         repository.Setup(obj => obj.AddAsync(It.IsAny<TItem>())).Callback((TItem item) =>
         {
             source.Add(item);
-            var identityValue = typeof(TKey) switch
-            {
-                Type t when t == typeof(int) => (TKey)(object)source.Count,
-                Type t when t == typeof(long) => (TKey)(object)source.Count,
-                Type t when t == typeof(Guid) => (TKey)(object)Guid.NewGuid(),
-                _ => default
-            };
-            item.GetType().GetProperty("Identity", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)!.SetValue(item, identityValue);
+            item.SetMockedIdentity<TItem, TKey>(source);
         }).Returns(Task.CompletedTask);
         repository.Setup(obj => obj.Update(It.IsAny<TItem>())).Callback((TItem item) => { source.Remove(item); source.Add(item); });
         repository.Setup(obj => obj.UpdateAsync(It.IsAny<TItem>())).Callback((TItem item) => { source.Remove(item); source.Add(item); }).Returns(Task.CompletedTask);
@@ -58,5 +47,24 @@ public static class RepositoryExtensions
         repository.Setup(obj => obj.Load(It.IsAny<TKey>())).Returns((TKey id) => source.FirstOrDefault(obj => obj.Identity!.Equals(id)));
         repository.Setup(obj => obj.LoadAsync(It.IsAny<TKey>())).Returns((TKey id) => Task.FromResult(source.FirstOrDefault(obj => obj.Identity!.Equals(id)))!);
         return repository;
+    }
+
+    private static void SetMockedIdentity<TItem, TKey>(this TItem item, IEnumerable<TItem> source)
+        where TItem : class, IAggregateRoot<TKey>
+    {
+        switch (typeof(TKey))
+        {
+            case Type t when t == typeof(int) && object.Equals(0, item.Identity):
+                item.GetType().GetProperty("Identity", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)!.SetValue(item, (TKey)(object)source.Count());
+                break;
+            case Type t when t == typeof(long) && object.Equals(0L, item.Identity):
+                item.GetType().GetProperty("Identity", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)!.SetValue(item, (TKey)(object)source.LongCount());
+                break;
+            case Type t when t == typeof(Guid) && object.Equals(Guid.Empty, item.Identity):
+                item.GetType().GetProperty("Identity", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)!.SetValue(item, Guid.NewGuid());
+                break;
+            default:
+                break;
+        }
     }
 }
