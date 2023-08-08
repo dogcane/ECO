6af0f41aafea7991309ad3aad28247ca9c3bb576
Andrea Canegrati <andrea@canegrati.net>
2023-08-08 09:59:01 +0200

    - NHibernate : query cache extension - HotChocolate : first integrations (WIP)

---
+++ src/ECO/Core/ECO.Integrations.HotChocolate/ECOBatchDataLoader.cs
@@ -0,0 +1,37 @@
+﻿using GreenDonut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECO.Integrations.HotChocolate
+{
+    public class ECOBatchDataLoader<T, K> : BatchDataLoader<K, T>
+        where T : class, IAggregateRoot<K>
+        where K : IEquatable<K>
+    {
+        #region Fields
+
+        IReadOnlyRepository<T, K> repository;
+
+        #endregion
+
+        #region Constructors
+
+        public ECOBatchDataLoader(IReadOnlyRepository<T, K> repository, IBatchScheduler batchScheduler, DataLoaderOptions? options = null) : base(batchScheduler, options)
+        {
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override async Task<IReadOnlyDictionary<K, T>> LoadBatchAsync(IReadOnlyList<K> keys, CancellationToken cancellationToken) 
+            => await Task.FromResult(repository.Where(agg => keys.Contains(agg.Identity)).ToDictionary(agg => agg.Identity, agg => agg));
+
+        #endregion
+    }
+}
---
+++ src/ECO/Core/ECO.Providers.NHibernate/Utils/NHQueryCacheExtensions.cs
@@ -0,0 +1,26 @@
+﻿using NHibernate.Linq;
+using System;
+using System.Linq;
+
+namespace ECO.Providers.NHibernate.Utils;
+
+public static class NHQueryCacheExtensions
+{
+    public static IQueryable<T> AsCachedQueryable<T, K>(this IReadOnlyRepository<T, K> repository, string regionName) where T : class, IAggregateRoot<K>
+    {
+        if (repository is null)
+        {
+            throw new ArgumentNullException(nameof(repository));
+        }
+        if (regionName is null)
+        {
+            throw new ArgumentNullException(nameof(regionName));
+        }
+
+        return repository.AsQueryable().WithOptions(opt =>
+        {
+            opt.SetCacheable(true);
+            opt.SetCacheRegion(regionName);
+        });
+    }
+}
